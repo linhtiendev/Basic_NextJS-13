e1631aa8438425ff4e49978dfe7c59d2def5ca01
linhtiendev <lelinhtien0707@gmail.com>
2023-09-04 17:24:13 +0700

    fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
-// import Image from 'next/image'
-// import styles from './page.module.css'
+"use client"
+
 import Link from "next/link";
 
-// dùng .module -> sẽ tạo ra class khác
-// tránh việc trùng class
 import x from '@/app/styles/appSe.module.css';
 import y from '@/app/styles/app.module.css';
 
 import AppTable from "@/app/components/Table";
-
-
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8000/blogs");
+      const data = await res.json()
+      console.log("check res", data);
+    }
+    fetchData();
+  }, [])
+
   return (
     <>
       {/* <main className={styles.main}>
